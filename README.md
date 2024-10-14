9e9e65561e1d3d30564183f0e8ebfea4b2e0dfb0
Pavol Calfa <pavol.calfa@external.t-systems.com>
2024-10-14 12:43:10 +0200

    new: Version 1.0

---
+++ README.md
@@ -0,0 +1,6 @@
+# OM0ET Quansheng based desktop transceiver menu controller
+
+Rotary encoder based menu controller for OM0ET Quansheng based desktop transceiver.
+
+For more details follow https://www.youtube.com/@OM0ET
+
--- om0et_quansheng_control/om0et_quansheng_control.ino
+++ om0et_quansheng_control/om0et_quansheng_control.ino
@@ -1,24 +1,10 @@
 // -----
-// SimplePollRotator.ino - Example for the RotaryEncoder library.
-// This class is implemented for use with the Arduino environment.
-//
-// Copyright (c) by Matthias Hertel, http://www.mathertel.de
-// This work is licensed under a BSD 3-Clause License. See http://www.mathertel.de/License.aspx
-// More information on: http://www.mathertel.de/Arduino
+// om0et_quansheng_contro.ino - Rotrary encoder based control of OM0ET Quansheng desktop transceiver
 // -----
-// 18.01.2014 created by Matthias Hertel
-// 04.02.2021 conditions and settings added for ESP8266
+// 13.10.2014 created by Pavol OM0WT
+// Created: "2024-10-13 21:25:38"
+// Last-Modified: "2024-10-14 12:42:40"
 // -----
-
-// This example checks the state of the rotary encoder in the loop() function.
-// The current position and direction is printed on output when changed.
-
-// Hardware setup:
-// Attach a rotary encoder with output pins to
-// * A2 and A3 on Arduino UNO.
-// * D5 and D6 on ESP8266 board (e.g. NodeMCU).
-// Swap the pins when direction is detected wrong.
-// The common contact should be attached to ground.
 
 #include <Arduino.h>
 #include <RotaryEncoder.h>
@@ -26,23 +12,6 @@
 #include "soc/soc.h"
 #include "soc/rtc_cntl_reg.h"
 
-#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_NANO_EVERY)
-// Example for Arduino UNO with input signals on pin 2 and 3
-#define PIN_IN1 D11
-#define PIN_IN2 D12
-
-#elif defined(ESP8266)
-// Example for ESP8266 NodeMCU with input signals on pin D5 and D6
-#define PIN_IN1 D5
-#define PIN_IN2 D6
-
-#endif
-// Arduino NANO
-//#define PIN_IN1 11
-//#define PIN_IN2 12
-//#define SW_PIN 10
-//#define PIN_UP 9
-//#define PIN_DOWN 8
 
 // Wemos MINI ESP32 D1
 #define PIN_ENC_CLK  35   // Encoder CLK
@@ -52,6 +21,7 @@
 #define PIN_OPT_DOWN 25   // IO25 - OPTO DOWN
 #define PIN_OPT_BTN  32   // IO32 - OPTO BUTTON 
 #define PIN_OPT_BTN_LONG 4 //IO04 - OPTO LONG PRESS BUTTON
+#define VERSION 1.0
 
 const int SHORT_PRESS_TIME = 1000; // 1000 milliseconds
 const int LONG_PRESS_TIME  = 1000; // 1000 milliseconds
@@ -61,10 +31,9 @@ unsigned long releasedTime = 0;
 bool isPressing = false;
 bool isLongDetected = false;
 bool isShortDetected = false;
-bool isControlling = false; // OPTO is controlling....
 bool switchIt = false;
 int controlPIN = 0;
-int optoDelay = 500;   // OTPO delay between ON and OFF
+int optoDelay = 100;   // OTPO delay between ON and OFF
 int dir = 0;
 
 // Setup a RotaryEncoder with 2 steps per latch for the 2 signal input pins:
@@ -76,7 +45,7 @@ void setup()
   button.setDebounceTime(70);
   Serial.begin(115200);
   while (! Serial);
-  Serial.println("SimplePollRotator example for the RotaryEncoder library.");
+  Serial.println("OM0ET Quansheng control");
   pinMode(PIN_OPT_BTN, OUTPUT);
   pinMode(PIN_OPT_UP, OUTPUT);
   pinMode(PIN_OPT_DOWN, OUTPUT);
@@ -118,15 +87,10 @@ void loop()
             digitalWrite(controlPIN, LOW);
         }
     }
-  //    Serial.print("pos:");
-  //    Serial.print(newPos);
-  //    Serial.print(" dir:");
-  //    Serial.println((int)(encoder.getDirection()));
-      
     pos = newPos; 
   } // if
 
-   if(button.isPressed()){
+  if(button.isPressed()){
     pressedTime = millis();
     isPressing = true;
     isLongDetected = false;
@@ -153,7 +117,6 @@ void loop()
 
   if(isPressing == true && isLongDetected == false) {
     long pressDuration = millis() - pressedTime;
-    
 
     if( pressDuration > LONG_PRESS_TIME ) {
       Serial.println("A long press is detected");
@@ -169,5 +132,4 @@ void loop()
   
   switchIt = false;
 } // loop ()
-
 // The End
